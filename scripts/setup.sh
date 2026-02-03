f71f86fe77e9d287b936e8e8e2dfbbe1de94f982
Rajesh Das <rajesh@hyperoot.dev>
2026-02-03 16:29:42 +0000

    chore: update project name in configuration files

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-/usr/local/bin/mise trust /workspaces/solution-template/mise.toml && /usr/local/bin/mise install
+/usr/local/bin/mise trust /workspaces/starter-template/mise.toml && /usr/local/bin/mise install
 sudo apt update && sudo apt install -y tmux python3
